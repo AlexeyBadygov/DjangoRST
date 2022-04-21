fa73b9662a18094a599e48880042f3579c508ccc
AlexeyBadygov <kick@mail.ru>
2022-04-21 13:29:04 +0700

    lesson_8

--- service/authors/tests.py
+++ service/authors/tests.py
@@ -1,8 +1,9 @@
 import json
 
-import mixer
+
 from django.contrib.auth.models import User
 from django.test import TestCase
+from mixer.backend.django import mixer
 
 from rest_framework import status
 from rest_framework.test import APIRequestFactory, force_authenticate, APIClient, APISimpleTestCase, APITestCase
